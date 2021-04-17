03b623b018a9598f8158ebec0cdac5f83de02c57
biancalina <bianca.alina1993@gmail.com>
2021-04-17 20:30:03 +0100

    install react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from "./WebcamCapture";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the snapchat App</h1>
-      <WebcamCapture />
+        <Router>
+          <div className='app__body'>
+            <Switch>
+              <Route exact path="/test">
+                <h1>Snapchat Clone</h1>
+              </Route>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
     </div>
   );
 }
