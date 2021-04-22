b7bb0f1dbacd1b6df03985d9673115b78183ca3a
biancalina <bianca.alina1993@gmail.com>
2021-04-22 18:03:36 +0100

    add more content

--- src/Preview.js
+++ src/Preview.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router';
+import { selectCameraImage } from './features/cameraSlice'
 import "./Preview.css"
 
 function Preview() {
+    const cameraImage = useSelector(selectCameraImage);
+    const history = useHistory()
+
+    useEffect(() => {
+        if (!cameraImage) {
+            history.replace('/')
+        }
+    }, [cameraImage, history])
+
     return (
         <div classNmae='preview'>
+            <h2>This is the preview</h2>
+            <img src={cameraImage} alt=""/>
 
         </div>
     )
