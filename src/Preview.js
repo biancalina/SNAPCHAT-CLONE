54bde9724c6cbd231030f438285afffcf31b92f3
biancalina <bianca.alina1993@gmail.com>
2021-04-24 18:03:11 +0100

    add more icons and start style them

--- src/Preview.js
+++ src/Preview.js
@@ -4,29 +4,44 @@ import { useHistory } from 'react-router';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import "./Preview.css";
 import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
-    const history = useHistory()
+    const history = useHistory();
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (!cameraImage) {
-            history.replace('/')
+            history.replace("/");
         }
     }, [cameraImage, history]);
     
     const closePreview = () => {
         dispatch(resetCameraImage());
-    }
+    };
 
     return (
-        <div classNmae='preview'>
-            <CloseIcon onClick={closePreview} classNme='preview__close'/>
-            <img src={cameraImage} alt=""/>
-
+        <div className="preview">
+            <CloseIcon onClick={closePreview} className="preview__close" />
+            <div className="preview__toolbarRight">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            </div>
+            <img src={cameraImage} alt="" />
         </div>
-    )
+    );
 }
 
-export default Preview
+export default Preview;
